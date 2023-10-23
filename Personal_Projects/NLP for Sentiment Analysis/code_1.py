a7e990a7e4d24416e8758c1b927773171165a29d
Lekhaz Adapa <lekhazadapa18@gmail.com>
2023-10-23 16:33:24 +0200

    gg

--- Personal_Projects/NLP for Sentiment Analysis/code_1.py
+++ Personal_Projects/NLP for Sentiment Analysis/code_1.py
@@ -11,42 +11,41 @@ def load_data(data_dir):
     labels = []
     for label in ['pos', 'neg']:
         folder = os.path.join(data_dir, label)
-        
-        # Check if the folder exists
-        if os.path.exists(folder) and os.path.isdir(folder):
-            for filename in os.listdir(folder):
-                with open(os.path.join(folder, filename), 'r', encoding='utf-8') as file:
-                    texts.append(file.read())
-                    labels.append(label)
-    
+        for filename in os.listdir(folder):
+            with open(os.path.join(folder, filename), 'r', encoding='utf-8') as file:
+                texts.append(file.read())
+                labels.append(label)
     return pd.DataFrame({'text': texts, 'label': labels})
 
-
-data_dir = '/Users/lekhmac/Documents/GitHub/Coding/Personal_Projects/NLP for Sentiment Analysis/aclImdb'  # Change to the path where your dataset is located
+data_dir = '/Users/lekhmac/Documents/GitHub/Coding/Personal_Projects/NLP for Sentiment Analysis/aclImdb'
 df_train = load_data(data_dir)
 
 # Step 2: Preprocess Data
 def preprocess_data(df):
-    df['text'] = df['text'].str.lower()  # Convert to lowercase
+    df['text'] = df['text'].str.lower()
     return df
 
 df_train = preprocess_data(df_train)
 
-# Step 3: Split Data
-X_train, X_val, y_train, y_val = train_test_split(df_train['text'], df_train['label'], test_size=0.2, random_state=42)
+# Step 3: Check if there are enough samples for both classes
+if df_train.groupby('label').size().min() < 2:
+    print("Error: Not enough samples in both classes.")
+else:
+    # Step 4: Split Data
+    X_train, X_val, y_train, y_val = train_test_split(df_train['text'], df_train['label'], test_size=0.2, random_state=42)
 
-# Step 4: Vectorize Text Data
-vectorizer = TfidfVectorizer(max_features=5000)
-X_train_vec = vectorizer.fit_transform(X_train)
-X_val_vec = vectorizer.transform(X_val)
+    # Step 5: Vectorize Text Data
+    vectorizer = TfidfVectorizer(max_features=5000)
+    X_train_vec = vectorizer.fit_transform(X_train)
+    X_val_vec = vectorizer.transform(X_val)
 
-# Step 5: Train Naive Bayes Classifier
-clf = MultinomialNB()
-clf.fit(X_train_vec, y_train)
+    # Step 6: Train Naive Bayes Classifier
+    clf = MultinomialNB()
+    clf.fit(X_train_vec, y_train)
 
-# Step 6: Make Predictions
-y_pred = clf.predict(vectorizer.transform(X_val))
+    # Step 7: Make Predictions
+    y_pred = clf.predict(vectorizer.transform(X_val))
 
-# Step 7: Evaluate Model
-accuracy = accuracy_score(y_val, y_pred)
-print(f'Accuracy: {accuracy:.2%}')
+    # Step 8: Evaluate Model
+    accuracy = accuracy_score(y_val, y_pred)
+    print(f'Accuracy: {accuracy:.2%}')
